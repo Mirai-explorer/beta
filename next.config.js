68147a893639e2893d6fb6964af4eceaa82a84df
Mirai-explorer <tangjw0226@gmail.com>
2023-10-17 03:14:37 +0800

    Project refactoring

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require('next-pwa')({
+    dest: 'public'
+})
 
-module.exports = nextConfig
+
+const nextConfig = {
+    output: 'standalone'
+}
+
+module.exports = withPWA(nextConfig)
